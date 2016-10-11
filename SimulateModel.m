73da40679e5492974547f30bf9799dd686426bee
benpolletta <benpolletta@gmail.com>
2016-10-11 16:10:10 -0400

    Removing excess code.

--- SimulateModel.m
+++ SimulateModel.m
@@ -458,9 +458,9 @@ if options.parallel_flag==1
   % simulations. This code circumvents this issue by assigning a different
   % study directory to each simulation.
   
-  % List any core files - these should be deleted, as they are huge (debug)
-  system (['ls ' fullfile(options.study_dir,'output*')],'-echo');
-  system('find * -name "core*"','-echo');
+%   % List any core files - these should be deleted, as they are huge (debug)
+%   system (['ls ' fullfile(options.study_dir,'output*')],'-echo');
+%   system('find * -name "core*"','-echo');
  
   % Generate unique* ID based on date and time (*as long as they dont start on the
   % exact same second...). Using these unique identfiers will now enable
