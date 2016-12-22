8f8d261bae7cb62c0db7df222f11a1a390d3ba95
David Stanley <davearthur.stanley@gmail.com>
2016-12-22 01:31:12 -0500

    CalcAverages.m: Updated to give the option of calculating standard deviation as well.

--- functions/CalcAverages.m
+++ functions/CalcAverages.m
@@ -1,18 +1,27 @@
-function data_out = CalcAverages(data)
-%% data_out = CalcFR(data,'option',value)
+function data_out = CalcAverages(data,opts)
+%% data_out = CalcAverages(data,'option',value)
 % Inputs:
 %   data - DynaSim data structure (see CheckData)
+%   opts - Options structure
+%          opts.save_std - Also creates new fields storing standard deviations (boolean; true/false)
+
 % Outputs:
-%   data_out: data structure all fields replaced by their average valeus
+%   data_out: data structure all fields replaced by their average vales
 %             (averaged across neurons).
 
 % See also: PlotFR, AnalyzeStudy, SimulateModel, CheckData, SelectVariables
 
 %% 1.0 Check inputs
 
+if nargin < 2
+    opts = struct;
+end
+
 data = CheckData(data);
 % note: calling CheckData() at beginning enables analysis function to
 % accept data matrix [time x cells] in addition to DynaSim data structure.
+
+opts = struct_addDef(opts,'save_std',0);
 
 %% do the averaging
 
@@ -22,9 +31,15 @@ for i = 1:length(data)
     labels = data(i).labels;
     labels = labels(~strcmp(labels,'time'));
     
-    % Sweep through these fields and take average
+    % Sweep through these fields and take averages
     for j = 1:length(labels)
+        % Save mean
         data_out(i).(labels{j}) = mean(data(i).(labels{j}),2);
+        
+        % Save standard deviation also if requested
+        if opts.save_std
+            data_out(i).([labels{j} '_std']) = std(data(i).(labels{j}),0,2);
+        end
     end
     
     % Set the size of all populations to 1
@@ -33,9 +48,6 @@ for i = 1:length(data)
     end
     
 end
-
-
-
 
 
 
