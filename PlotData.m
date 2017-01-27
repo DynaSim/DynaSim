da5646516c06ae2f85a319fd978d9ed1476cdecb
benpolletta <benpolletta@gmail.com>
2017-01-27 18:01:09 -0500

    Applying plotdata_multivaried patch.

--- PlotData.m
+++ PlotData.m
@@ -137,6 +137,72 @@ data=CheckData(data);
   % accept data matrix [time x cells] in addition to DynaSim data structure.
 fields=fieldnames(data);
 
+vary_labels = data(1).varied; % data(1).simulator_options.vary;
+
+no_vary_labels = length(vary_labels);
+
+vary_params = nan(length(data), no_vary_labels);
+
+vary_vectors = cell(no_vary_labels, 1);
+
+vary_lengths = nan(no_vary_labels, 1);
+
+for v = 1:no_vary_labels
+   
+    vary_params(:, v) = [data.(vary_labels{v})];
+    
+    vary_vectors{v} = unique(vary_params(:, v));
+    
+    vary_lengths(v) = length(vary_vectors{v});
+    
+end
+
+no_varied = sum(vary_lengths > 1);
+
+if no_varied > 2
+    
+    no_figures = prod(vary_lengths(3:end));
+    
+    data = reshape(data, prod(vary_lengths(3:end)), prod(vary_lengths([1 2])));
+    
+    figure_params = nan(no_vary_labels - 2, 1);
+    
+    vary_lengths_cp = cumprod(vary_lengths);
+    
+    for f = 1:no_figures
+        
+        figure_params(1) = vary_vectors{3}(mod(f - 1, vary_lengths(3)) + 1);
+        
+        for v = 4:no_vary_labels
+            
+            figure_params(v - 2) = vary_vectors{v}(ceil(f/vary_lengths_cp(v - 3)));
+            
+        end
+        
+        vary_title = '';
+        
+        for v = 1:(no_vary_labels - 2)
+            
+            vary_title = [vary_title, sprintf('%s = %f ', vary_labels{v + 2}, figure_params(v))];
+            
+        end
+    
+        handles = PlotData(data(f, :), varargin{:});
+        
+        for h = 1:length(handles)
+        
+            % figure(handles(h))
+    
+            mtit(handles(h), vary_title, 'FontSize', 14, 'yoff', .2)
+       
+        end
+        
+    end
+    
+    return
+    
+end
+
 options=CheckOptions(varargin,{...
   'time_limits',[-inf inf],[],...
   'variable',[],[],...        
