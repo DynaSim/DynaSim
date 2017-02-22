91ef1296ca5405b5924a17ff661a408364c31b1c
erikthered12 <eriktheblue@gmail.com>
2017-02-22 12:29:02 -0500

    generalize prefix for nameFromVaried

--- functions/nameFromVaried.m
+++ functions/nameFromVaried.m
@@ -1,18 +1,18 @@
-function new_result_file = nameFromVaried(data, plot_type, old_result_file)
-%% new_result_file = nameFromVaried(data, plot_type, old_result_file)
+function new_result_file = nameFromVaried(data, file_type, old_result_file)
+%% new_result_file = nameFromVaried(data, file_type, old_result_file)
 % purpose: makes a filename based on the parameters in data.varied.
 %
 % inputs:
 %   data: DynaSim data structure (also accepted: data file name)
-%   plot_type: what to plot
+%   file_type: file prefix for type of file
 %   result_file: previous result_file, only uses this for the path.
 %
 % outputs:
-%   new_result_file: where to save result, based on plot_type and data.varied
+%   new_result_file: where to save result, based on file_type and data.varied
 
 pathstr = fileparts(old_result_file);
 
-fileName = plot_type;
+fileName = file_type;
 
 %check for simID# from batch sims
 token = regexp(old_result_file, '(sim\d+)', 'tokens');
