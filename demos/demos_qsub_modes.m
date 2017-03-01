92d7047df9663203baa24f9027a6442bcf1068f0
erikthered12 <eriktheblue@gmail.com>
2017-03-01 18:41:33 -0500

    add qsub mode demo

---
+++ demos/demos_qsub_modes.m
@@ -0,0 +1,40 @@
+% Below are 2 different ways to simulate on the cluster.  With larger
+% simulations (>100s), array mode should be faster.
+
+%% Setup
+demos_path = findDemosPath;
+
+% Set path to your copy of the DynaSim toolbox
+dynasim_path = fullfile(demos_path, '..');
+
+% add DynaSim toolbox to Matlab path
+addpath(genpath(dynasim_path)); % comment this out if already in path
+
+% Set where to save outputs
+output_directory = fullfile(demos_path, 'outputs');
+% move to root directory where outputs will be saved
+cd(output_directory);
+
+%% Solve with loop mode
+study_dir='study_HH_varyI_cluster_loop'; 
+eqns='dv/dt=@current+I; {iNa,iK}';
+vary={'','I',[0:10:50]};
+
+if exist(study_dir, 'dir')
+  rmdir(study_dir, 's')
+end
+
+SimulateModel(eqns,'vary',vary, 'study_dir',study_dir,'save_data_flag',1,...
+  'cluster_flag',1,'verbose_flag',1,'qsub_mode','loop');
+
+%% Solve with array mode
+study_dir='study_HH_varyI_cluster_array'; 
+eqns='dv/dt=@current+I; {iNa,iK}';
+vary={'','I',[0:10:50]};
+
+if exist(study_dir, 'dir')
+  rmdir(study_dir, 's')
+end
+
+SimulateModel(eqns,'vary',vary, 'study_dir',study_dir,'save_data_flag',1,...
+  'cluster_flag',1,'verbose_flag',1,'qsub_mode','array');
