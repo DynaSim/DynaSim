4a05d527ab158d2d1e0d2a545178e42342e2c0cc
David Stanley <davearthur.stanley@gmail.com>
2017-03-03 01:11:08 -0500

    Did some work on demos_xPlt.m

--- demos/demos_xPlt.m
+++ demos/demos_xPlt.m
@@ -1,4 +1,4 @@
-%% Run simulation - Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
+%% Set up paths 
 % Get ready...
 
 demos_path = findDemosPath;
@@ -13,6 +13,8 @@ addpath(genpath(dynasim_path)); % comment this out if already in path
 output_directory = fullfile(demos_path, 'outputs');
 % move to root directory where outputs will be saved
 cd(output_directory);
+
+%% Run simulation - Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
 
 % define equations of cell model (same for E and I populations)
 eqns={ 
@@ -49,59 +51,109 @@ SimulateModel(s,'save_data_flag',1,'study_dir','demo_sPING_3b',...
                 'vary',vary,'verbose_flag',1, ...
                 'save_results_flag',1,'plot_functions',@PlotData,'plot_options',{'format','png'} );
 
-cd ..
 
-%% Load the data and import into nDDict class
-% ...Assumes we have some DynaSim data already loaded...
-cd outputs
+
+%% Load the data and import into xPlt class
+
+% Load data in traditional DynaSim format
 data=ImportData('demo_sPING_3b');
-cd ..
 
-% Load the data linearly
+% Extract the data in a linear format
 [data_linear,ax,ax_names,time] = DynaSimExtract (data);
 
 
-% Import into an xPlt class
+
+% The xPlt class inherits from the multidimensional dictionaries (nDDict)
+% class and adds some plotting functionality.
 xp = xPlt;
+
+% Import linear data into xp
 xp = xp.importLinearData(data_linear,ax{:});
 xp = xp.importAxisNames(ax_names);
+
+% At its core, xPlt has 3 fields. xp.data stores the actual data (either a 
+% matrix or a cell array). xp.axis stores axis labels associated with each
+% dimension of the data. xp.misc stores meta data for use by the user as they
+% see fit.
+disp(xp);
+
+
+% Viewing contents of xp.axis. Values field stores axis labels. Can be
+% numeric...
+disp(xp.axis(1));
+
+% ...or string type. Name field stores the name of the dimension. astruct
+% is for internal use.
+disp(xp.axis(4));
+
+% Add some custom info to xp.metadata. This can be whatever you want.
 meta = struct;
-meta.datainfo(1:2) = nDDictAxis;      % Use nDDictAxis here, because why not?
+meta.datainfo(1:2) = nDDictAxis;
 meta.datainfo(1).name = 'time(ms)';
 meta.datainfo(1).values = time;
 meta.datainfo(2).name = 'cells';
 meta.datainfo(2).values = [];
 xp = xp.importMeta(meta);
+clear meta
 
 
-%% Run another recursive plot
+
+%% Validate & get some properties of the data
+xp.checkDims;       % Makes sure all the dimensions match up (e.g. xp.axis 
+                    % must have the same length as number of dimensions in
+                    % xp.data, and the size of each dimension must match
+                    % the number of labels
+                    
+xp.fixAxes;         % This attempts to automatically fix any dimension
+                    % mismatches between xp.data and xp.axis. This
+                    % particular command does nothing because there are no
+                    % errors in xp
+
+                    
+% Make an xPlt class with errors.
+disp(size(xp.data));                % The 4th dimension of xp.data is of size 8
+disp(xp.axis(4).values);            % Originally it has 8 labels
+xp_bad = xp; 
+xp_bad.axis(4).values={'test'};     % Reduce this to 1 (mismatch)
+
+% Check errors in new class
+% xp_bad.checkDims;
+
+% Auto fix errors in labels
+xp_fixed = xp_bad.fixAxes; 
+
+% View new labels
+xp_fixed.axis(4).values         % The original cell array had been replaced by 
+                                % one of appropraite length
+                                
+% View summary of the classes. Tells the dimensionality of xp.data and also
+% the number of labels in each axis.
+xp.getaxisinfo
+xp_fixed.getaxisinfo
+
+%% Plot for 2D parameter sweep
 clear xp2 xp3
-xp4 = xp.subset([],[],1,8);
+xp4 = xp.subset([],[],1,8);         % Eliminate all but 2 dimensions for xp4.
 xp4.getaxisinfo
 
-% recursivePlot(xp4,{@xp_subplot,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{[],1},{1,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{},{0,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
-recursivePlot_2(xp4,{@xp_subplot,@xp_matrix_basicplot},{[1,2]},{{0,0},{}});
-
+% Tip: don't try to understand what recursivePlot is doing - instead, try
+% putting break points in the various function handles to see how this
+% command works.
 recursivePlot(xp4,{@xp_subplot,@xp_matrix_basicplot},{[1,2]},{{0,0},{}});
 
 
 
-%% Run another recursive plot
+%% Plot for 3D data
 clear xp2 xp3
 xp4 = xp.subset([],[],[],8);
 xp4.getaxisinfo
 
-% recursivePlot(xp4,{@xp_subplot,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{[],1},{1,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{},{0,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
 recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
 
 
 
 
-%% Run another recursive plot
+%% Plot for 3D data (larger one)
 clear xp2 xp3
 xp4 = xp.subset([],[],1,[]);
 xp4 = xp4.squeeze;
@@ -114,28 +166,14 @@ recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
 
 
 
-%% Run a recursive plot
+%% Use nested images to fit 3D data into a 2D subplot
 
 clear xp2 xp3
 xp4 = xp.subset([],[],[],8);
 xp4.getaxisinfo
 
-% recursivePlot(xp4,{@xp_subplot,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{[],1},{1,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{},{0,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
 recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot_grid3D,@xp_matrix_basicplot},{[1,2,4],3},{{},{0,1},{}});
-
-
-%% Run a recursive plot
-
-clear xp2 xp3
-xp4 = xp.subset([],[],[],8);
-xp4.getaxisinfo
-
-% recursivePlot(xp4,{@xp_subplot,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{[],1},{1,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{1:2,3},{{},{0,1},{}});
-% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_matrix_basicplot},{[3,1,2]},{{},{}});
-recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{[1,2,4],3},{{},{0,1},{}});
+% recursivePlot(xp4,{@xp_subplot_grid3D,@xp_subplot,@xp_matrix_basicplot},{[1,2,4],3},{{},{0,1},{}});
 
 
 %% Test subset selection using regular expressions
