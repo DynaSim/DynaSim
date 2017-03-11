f4ecee6209a17b161f363566d5b31033e420b565
David Stanley <davearthur.stanley@gmail.com>
2017-03-11 16:57:32 -0500

    Deleted functions_xPlt now that I have imported my submodule, MDD

--- functions_xPlt/classes/@xPlt/xPlt.m
+++
@@ -1,9 +0,0 @@
-
-
-classdef xPlt < nDDict
-    
-    % xPlt class inherets from the nDDict class (AKA multidimensional
-    % dictionaries). xPlt adds some plotting functionality (see
-    % xPlt.RecursivePlot)
-    
-end
