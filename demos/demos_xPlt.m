f57d1f3c38bfeed0ad074e9467b34fc32a1af390
David Stanley <davearthur.stanley@gmail.com>
2017-03-11 17:25:08 -0500

    Added placeholder demos_xPlt.m to demos folder.

---
+++ demos/demos_xPlt.m
@@ -0,0 +1,34 @@
+%% % % % % % % % % % % % % % % SETTING UP xPlt OBJECT % % % % % % % % % %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+%% Set up paths 
+% Get ready...
+
+% Format
+format compact
+
+% Check if in right folder
+[parentfolder,currfolder] = fileparts(pwd);
+if ~strcmp(currfolder,'demos'); error('Should be in demos folder to run this code.'); end
+
+% Set path to your copy of the DynaSim toolbox
+dynasim_path = fullfile(parentfolder);
+
+% add DynaSim toolbox to Matlab path
+addpath(genpath(dynasim_path)); % comment this out if already in path
+
+% Set where to save outputs
+output_directory = fullfile(pwd, 'outputs');
+study_dir = fullfile(output_directory,'demo_sPING_3b');
+
+% move to root directory where outputs will be saved
+mkdir_silent(output_directory);
+% cd(fullfile(dynasim_path, output_directory));
+
+fprintf('Note1 - I am planning to rename nDDict to MDD (MultiDimensional Dictionary).\n');
+fprintf('Note2 - I have moved the rest of this demos script to the ../MDD now in\n ');
+fprintf('order for it to updated in sync with the MDD repo.\n');
+
+% Open demos_xPlt.m script
+edit ../MDD/demos_xPlt.m
+
