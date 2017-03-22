1791d3ccede1a2b56bf0d4b854590387234c604b
David Stanley <davearthur.stanley@gmail.com>
2017-03-22 12:05:08 -0400

    #130: Added demo script for PlotData2 and also demo script for generating small DynaSim datasets for use in tutorials.

---
+++ demos/demos_PlotData2.m
@@ -0,0 +1,126 @@
+%% % % % % % % % PlotData2 tutorial % % % % % % % %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+%% Set up paths 
+% Get ready...
+
+% Format
+format compact
+clear all
+restoredefaultpath
+
+% Check if in right folder
+[parentfolder,currfolder] = fileparts(pwd);
+if ~strcmp(currfolder,'demos'); error('Should be in demos folder to run this code.'); end
+
+% add DynaSim toolbox to Matlab path
+addpath(genpath(parentfolder)); % comment this out if already in path
+
+
+            
+%% Load sample DynaSim data
+
+% Load some sample simulated data
+load('sample_data_dynasim.mat');
+
+%% Do some basic plots with PlotData2
+
+% Default settings
+PlotData2(data);
+
+% Take mean
+PlotData2(data,'do_mean',1);
+
+% Plot data, just E cells, with zoom turned on
+PlotData2(data,'do_zoom',1,'population','E');
+
+% As above, but focus on only a subset of the varied data. Also, reduce the
+% total number of traces displayed.
+PlotData2(data,'population','E','variable','v','E_Iapp',1:3,'varied2',[1,3],'do_zoom',1,'max_num_overlaid',3);
+
+%% Recursive plots with PlotData2
+
+% The number of subplots packed into a single figure can be substantially
+% increased by recurisve subplotting. This is controlled with the
+% "num_embedded_subplots" flag, which can range from 1-4.
+
+% Plot membrane voltage for E and I cells across the parameter sweep
+% % (1 variable, 2 pops, varied1, varied2: Ndims = 3)
+PlotData2(data,'num_embedded_subplots',2,'do_zoom',1);  % Default
+PlotData2(data,'num_embedded_subplots',4,'do_zoom',1);  % Increase embedding
+
+% Note that in the prev example, although we set embedded subplots to 4, only
+% the first 3 are used. If excess subplots are requested and unused, they
+% will be thrown out.
+
+% Setting num_embedded_subplots to provides the same data but in a different
+% arrangement.
+PlotData2(data,'num_embedded_subplots',3,'do_zoom',1);
+
+% Depending on the data being plotted, different some arrangements can be
+% more useful than others. For example, this plot embeds varied as a
+% sub-subplot.
+% % (2 variables, 2 pops, varied2, (varied1 is fixed): Ndims = 3)
+PlotData2(data,'num_embedded_subplots',3,'variable','iNa*','varied1',2)
+
+% Lastly, 4 subplots can be nested together to view 4 dimensions
+% simultaneously. This can be increased to 5 with the overlay function
+% described below, and 6 if you count multiple figures. While these figures
+% may get crowded, the supersize_me flag, also described below, aims to 
+% address this by producing single figures occupying very large canvases,
+% which can be zoomed to a high level of detail.
+% % (2 vars, 2 pops, varied1, varied2: Ndims = 4)
+PlotData2(data,'max_num_overlaid',3,'num_embedded_subplots',4,'population','all','variable','v|iNa_h','varied1',2:3,'lock_axes',false);
+
+
+%% Modifying overlaid traces
+
+% This provides an alternative method for stacking more data into a single
+% plot.
+
+% If there is only 1 cell from each population, PlotData will overlay
+% cells from different populations by default.
+% This can be achieved by averaging across cells...
+PlotData2(data,'do_mean',1,'varied1',1:3,'varied2',2:3)
+
+% ... or by looking at only the 1st cell in each population
+PlotData2(data,'max_num_overlaid',1,'varied1',1:3,'varied2',2:3)
+
+% Turning this overlay off splits up the figures
+PlotData2(data,'max_num_overlaid',1,'varied1',1:3,'varied2',2:3,'force_overlay','none')
+
+% Overlays can also be doubly stacked
+PlotData2(data,'max_num_overlaid',10,'force_overlay','populations');
+
+% However, these can be messy, so the groups can be shifted up or down
+PlotData2(data,'max_num_overlaid',10,'force_overlay','populations','do_overlay_shift',true);
+    % Variables are stacked from top to bottom, so E cells are on top and I
+    % cells underneath
+
+% You can overlay any variable. For example, here is injected current
+PlotData2(data,'do_mean',0,'varied1',1:3,'varied2',2:3,'force_overlay','varied1','do_overlay_shift',1,'overlay_shift_val',100)
+
+
+% Variables with very different units can be compared side-by-side by
+% taking the z-score first. In this case we compare E cell membrane voltage
+% to its inhibitory synaptic input.
+PlotData2(data,'population','E','variable','v|I_iGABAa_s','force_overlay','variables','do_overlay_shift',true,'overlay_shift_val',3,'do_zscore',true,'do_zoom',1);
+
+% % Double stack overlays
+% PlotData2(data,'population','E','variable','iNa_m|I_iGABAa_s','force_overlay','variables','do_overlay_shift',true);
+% PlotData2(data,'population','I','variable','v');
+
+
+%% Import pre-saved images
+
+% Import pre-saved images and tile them
+PlotData2('../../outputs/demo_sPING_3b/')
+
+% As above, but supersize them
+PlotData2('../../outputs/demo_sPING_3b/','supersize_me',1)
+
+
+%% Supersize me
+load sample_data_dynasim_large.mat
+
+PlotData2(data);
---
+++ demos/demos_generate_data.m
@@ -0,0 +1,102 @@
+%% Generate data
+% This is a script for generating small sets of dat for use in testing. It
+% saves the data in .mat files, which seems to be a bit more efficient than
+% saving as a DynaSim path
+
+
+%% Set up paths 
+% Get ready...
+
+% Format
+format compact
+clear all
+restoredefaultpath
+
+% Check if in right folder
+[parentfolder,currfolder] = fileparts(pwd);
+if ~strcmp(currfolder,'demos'); error('Should be in demos folder to run this code.'); end
+
+% add DynaSim toolbox to Matlab path
+addpath(genpath(parentfolder)); % comment this out if already in path
+
+% Study directory
+output_directory = fullfile(parentfolder,'outputs');
+study_dir = fullfile(output_directory,'demo_sPING_3b_2plots');
+
+
+%% Generate a huge dataset
+% Run simulation - Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
+
+% define equations of cell model (same for E and I populations)
+eqns={ 
+  'dv/dt=Iapp+@current+noise*randn(1,N_pop)';
+  'monitor iGABAa.functions, iAMPA.functions'
+};
+% Tip: monitor all functions of a mechanism using: monitor MECHANISM.functions
+
+% create DynaSim specification structure
+s=[];
+s.populations(1).name='E';
+s.populations(1).size=80;
+s.populations(1).equations=eqns;
+s.populations(1).mechanism_list={'iNa','iK'};
+s.populations(1).parameters={'Iapp',5,'gNa',120,'gK',36,'noise',40};
+s.populations(2).name='I';
+s.populations(2).size=20;
+s.populations(2).equations=eqns;
+s.populations(2).mechanism_list={'iNa','iK'};
+s.populations(2).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',40};
+s.connections(1).direction='I->E';
+s.connections(1).mechanism_list={'iGABAa'};
+s.connections(1).parameters={'tauD',10,'gSYN',.1,'netcon','ones(N_pre,N_post)'};
+s.connections(2).direction='E->I';
+s.connections(2).mechanism_list={'iAMPA'};
+s.connections(2).parameters={'tauD',2,'gSYN',.1,'netcon',ones(80,20)};
+
+% Vary two parameters (run a simulation for all combinations of values)
+vary={
+  'E'   ,'Iapp',[0:10:10];      % amplitude of tonic input to E-cells
+  %'I'   ,'Iapp',[0 5 10];      % amplitude of tonic input to E-cells
+  'I->E','tauD',[5:5:5]       % inhibition decay time constant from I to E
+  };
+SimulateModel(s,'save_data_flag',1,'study_dir',study_dir,...
+                'vary',vary,'verbose_flag',1, 'downsample_factor', 10, ...
+                'save_results_flag',1,'plot_functions',{@PlotData,@PlotData},'plot_options',{{'format','png','visible','off','figwidth',0.5,'figheight',0.5}, ...
+                {'format','png','visible','off','plot_type','rastergram','figwidth',0.5,'figheight',0.5}} );
+
+%% Load the data
+
+% Load data in traditional DynaSim format
+data=ImportData(study_dir);
+
+% Import the data images
+
+% Import plot files
+data_img = ImportPlots(study_dir);
+
+
+save('sample_data_dynasim_2plots.mat','data','data_img');
+
+%% 
+
+
+% Load into DynaSim structure
+[data_table,column_titles] = DataField2Table (data_img,'plot_files');
+
+% Preview the contents of this table
+previewTable(data_table,column_titles);
+
+% The entries in the first column contain the paths to the figure files.
+% There can be multiple figures associated with each simulation, which is
+% why these are cell arrays of strings.
+disp(data_table{1}{1})
+disp(data_table{1}{2})
+
+% Import the linear data into an xPlt object
+xp_img = xPlt;
+X = data_table{1}; axislabels = data_table(2:end);
+xp_img = xp_img.importLinearData(X, axislabels{:});
+xp_img = xp_img.importAxisNames(column_titles(2:end));
+
+
+
