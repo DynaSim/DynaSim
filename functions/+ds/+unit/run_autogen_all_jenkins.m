124c839dec826fb0be40d85626f6d208e1488f6d
Erik Roberts <eriktheblue@gmail.com>
2017-05-02 15:26:23 -0400

    add renaming to autogen all jenkins

--- functions/+ds/+unit/run_autogen_all_jenkins.m
+++ functions/+ds/+unit/run_autogen_all_jenkins.m
@@ -17,6 +17,20 @@ fprintf('Workspace:%s\n',ws);
 %% Make Default Config
 ds.makeDefaultConfigJenkins;
 
+%% Rename autogen_newSave to autogen
+finalDir = fullfile(ds.getConfig('ds_unitTestData_path'), 'autogen');
+if ~exist(finalDir, 'dir')
+  newDir = fullfile(ds.getConfig('ds_unitTestData_path'), 'autogen_newSave');
+  movefile(newDir, finalDir);
+end
+
+%% Rename autogenDirs_newSave to autogenDirs
+finalDir = fullfile(ds.getConfig('ds_unitTestData_path'), 'autogenDirs');
+newDir = fullfile(ds.getConfig('ds_unitTestData_path'), 'autogenDirs_newSave');
+if ~exist(finalDir, 'dir') && exist(newDir, 'dir')
+  movefile(newDir, finalDir);
+end
+
 %% Make Test Suite
 fullSuite = TestSuite.fromPackage('ds.unit');
 fullSuite = fullSuite.selectIf(~HasTag('query'));
