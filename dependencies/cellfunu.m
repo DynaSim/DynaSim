96129184a0a687d76911b5fc04fc504557623a95
Erik Roberts <eriktheblue@gmail.com>
2017-05-10 11:19:10 -0400

    fix cellfunu to not have varargout

--- dependencies/cellfunu.m
+++ dependencies/cellfunu.m
@@ -1,10 +1,9 @@
-function varargout = cellfunu (varargin)
+function output = cellfunu(varargin)
 %CELLFUNU - Wraps cellfun, with uniform output set to zero
 %
 % Usage:
 %   out = cellfunu (varargin)
 
-varargout = cell(1,nargout);
-[varargout{1:nargout}] = cellfun(varargin{:},'UniformOutput',0);
+output = cellfun(varargin{:},'UniformOutput',0);
 
 end
