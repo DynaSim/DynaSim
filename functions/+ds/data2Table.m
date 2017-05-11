d506b0daf2a6bbac03c5c9df6af63b25fa8ea1a0
David Stanley <davearthur.stanley@gmail.com>
2017-05-11 02:47:15 -0400

    iss #173 Updated all references to xPlt and nDDict to be MDict.

--- functions/+ds/data2Table.m
+++ functions/+ds/data2Table.m
@@ -1,6 +1,6 @@
 function [data_table,column_titles,time] = data2Table(data,verbose_flag)
     % Converts DynaSim structure to 1D cell array format. Later can use to
-    % import to xPlt
+    % import to MDict
     
     if nargin < 2
         verbose_flag = 0;
