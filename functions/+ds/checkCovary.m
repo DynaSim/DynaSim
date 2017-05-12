72e04a9a3e81d508c20718bfb789919f9d36b621
benp <benpolletta@gmail.com>
2017-05-12 17:29:27 -0400

    checkCovary: removing check for equal vary length blocks before searching for covaried parameters (just does all of them); flagging all indices for removal before removing any of them.

--- functions/+ds/checkCovary.m
+++ functions/+ds/checkCovary.m
@@ -21,46 +21,34 @@ non_singleton_vary_lengths = vary_lengths(vary_lengths > 1);
 
 non_singleton_vary_params = vary_params(:, vary_lengths > 1);
 
-diff_vary_lengths = diff(non_singleton_vary_lengths);
-
-equal_vary_length_indices = diff_vary_lengths == 0;
-
-equal_vary_length_blocks = ds.indexToBlocks(equal_vary_length_indices);
-
-if ~isempty(equal_vary_length_blocks)
-  equal_vary_length_blocks(:, 2) = equal_vary_length_blocks(:, 2) + 1;
-end
-
-no_blocks = size(equal_vary_length_blocks, 1);
-
 linked_indices = {};
 
 number_linked_sets = 0;
-
-for block = 1:no_blocks
-  param_indices = equal_vary_length_blocks(block, 1):equal_vary_length_blocks(block, 2);
   
-  iteration_number = 1;
-  while length(param_indices) > 1
+iteration_number = 1;
+while length(param_indices) > 1
     [linked_set, param_indices] = find_linked_params(param_indices, non_singleton_vary_params, varargin{:});
     
     if ~isempty(linked_set)
-      number_linked_sets = number_linked_sets + 1;
-      linked_indices{number_linked_sets} = linked_set;
+        number_linked_sets = number_linked_sets + 1;
+        linked_indices{number_linked_sets} = linked_set;
     end
     
     iteration_number = iteration_number + 1;
     if iteration_number > 10
-      return
+        return
     end
-  end
 end
 
 effective_vary_lengths = non_singleton_vary_lengths;
 
+marked_for_removal = zeros(size(effective_vary_lengths));
+
 for l = 1:number_linked_sets
-  effective_vary_lengths(linked_indices{l}(2:end)) = [];
+  marked_for_removal(linked_indices{l}(2:end)) = 1;
 end
+
+effective_vary_lengths(marked_for_removal) = [];
 
 %% auto_gen_test_data_flag argout
 if options.auto_gen_test_data_flag
