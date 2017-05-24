4fa5e1eebe9518bf36016712887c1cdb34315aee
benp <benpolletta@gmail.com>
2017-05-23 21:35:06 -0400

    checkCovary2: returning to checkCovary mechanism for the parameters that are involved in one or more 'full rank' choice of parameters.

--- functions/+ds/checkCovary2.m
+++ functions/+ds/checkCovary2.m
@@ -1,4 +1,4 @@
-function [effective_vary_indices, linked_indices] = checkCovary2(vary_lengths, data_length, varargin)
+function [effective_vary_indices, linked_indices] = checkCovary2(vary_lengths, vary_params, data_length, varargin)
   %CHECKCOVARY - TODO I assume this checks if any varied parameters are covaried?
  
   %% localfn output
@@ -18,6 +18,7 @@ function [effective_vary_indices, linked_indices] = checkCovary2(vary_lengths, d
 
   % Remove any vary statements used to set parameter values.
   non_singleton_vary_lengths = vary_lengths(vary_lengths > 1);
+  non_singleton_vary_params = vary_params(:, vary_lengths > 1);
   n_varied = length(non_singleton_vary_lengths);
 
   %% Check all combinations of varied parameters to see if the product of the number
@@ -41,8 +42,59 @@ function [effective_vary_indices, linked_indices] = checkCovary2(vary_lengths, d
     end
 
   end
+
+  linked_indices = {};
   
-  linked_indices = find_linked_params(full_rank_choices, length(non_singleton_vary_lengths), varargin{:});
+  %% Parse the results of finding "full rank" choices.
+  if isempty(full_rank_choices)
+    % No combination of varied parameters equals total data length,
+    % so treat data as linear.
+    linked_indices{1} = 1:n_varied;
+
+  else
+    % If one or more combinations of varied parameters describes the total data length,
+    % we need to link covaried parameters.
+    no_full_rank_choices = length(full_rank_choices);
+
+    % Create a matrix in which each row represents a full rank choice of varied parameters.
+    full_rank_participation = zeros(no_full_rank_choices, n_varied);
+    for frc = 1:no_full_rank_choices
+      full_rank_participation(frc, full_rank_choices{frc}) = 1;
+    end
+    
+    % If a certain parameter is not involved in any "full rank" product,
+    % consider it linked.
+    index = 1;
+    non_participating_parameters = find(sum(full_rank_participation, 1) == 0);
+    if ~isempty(non_participating_parameters)
+        linked_indices{index} = [0 non_participating_parameters];
+        index = index + 1;
+    end
+
+    if no_full_rank_choices > 1
+        % If more than one combination of varied parameters describes the total
+        % data length, we have to figure out which of these varied
+        % parameters is co-varied.
+        param_indices = find(sum(full_rank_participation, 1) > 0);
+        
+        iteration_number = 1;
+        while length(param_indices) > 1
+            [linked_set, param_indices] = find_linked_params(param_indices, non_singleton_vary_params, varargin{:});
+            
+            if ~isempty(linked_set)
+                linked_indices{index} = linked_set;
+                index = index + 1;
+            end
+            
+            iteration_number = iteration_number + 1;
+            if iteration_number > 10
+                return
+            end
+        end
+        
+    end
+
+  end
 
   % Remove linked sets of parameters from effective_vary_lengths.
   marked_for_removal = zeros(size(non_singleton_vary_lengths));
@@ -62,73 +114,43 @@ function [effective_vary_indices, linked_indices] = checkCovary2(vary_lengths, d
 
 end
 
-function linked_indices = find_linked_params(full_rank_choices, no_varied, varargin)
+function [linked_indices, non_linked_indices] = find_linked_params(param_indices, vary_params, varargin)
 
-  %% auto_gen_test_data_flag argin
-  options = ds.checkOptions(varargin,{'auto_gen_test_data_flag',0,{0,1}},false);
-  if options.auto_gen_test_data_flag
-    varargs = varargin;
-    varargs{find(strcmp(varargs, 'auto_gen_test_data_flag'))+1} = 0;
-    varargs(end+1:end+2) = {'unit_test_flag',1};
-    argin = [{full_rank_choices}, varargs];
-  end
+%% auto_gen_test_data_flag argin
+options = ds.checkOptions(varargin,{'auto_gen_test_data_flag',0,{0,1}},false);
+if options.auto_gen_test_data_flag
+  varargs = varargin;
+  varargs{find(strcmp(varargs, 'auto_gen_test_data_flag'))+1} = 0;
+  varargs(end+1:end+2) = {'unit_test_flag',1};
+  argin = [{param_indices},{vary_params}, varargs];
+end
 
-  linked_indices = {};
+param1_index = param_indices(1);
+
+params_at_value1 = vary_params(vary_params(:, param1_index) == vary_params(1, param1_index), :);
+
+no_values_at_value1 = nan(size(param_indices));
+
+no_values_at_value1(1) = 1;
+
+for p = 2:length(param_indices)
   
-  %% Parse the results of finding "full rank" choices.
-  if isempty(full_rank_choices)
-    % No combination of varied parameters equals total data length,
-    % so treat data as linear.
-    linked_indices{1} = 1:no_varied;
-
-  else
-    % If one or more combinations of varied parameters describes the total data length,
-    % we need to link covaried parameters.
-    no_full_rank_choices = length(full_rank_choices);
-
-    % Create a matrix in which each row represents a full rank choice of varied parameters.
-    full_rank_participation = zeros(no_full_rank_choices, no_varied);
-    for frc = 1:no_full_rank_choices
-      full_rank_participation(frc, full_rank_choices{frc}) = 1;
-    end
-    
-    % If a certain parameter is not involved in any "full rank" product,
-    % consider it linked.
-    index = 1;
-    non_participating_parameters = find(sum(full_rank_participation, 1) == 0);
-    if ~isempty(non_participating_parameters)
-        linked_indices{index} = [0 non_participating_parameters];
-        index = index + 1;
-    end
-
-    if no_full_rank_choices > 1
-        % If more than one combination of varied parameters describes the total
-        % data length, for each pair of full rank choices, see if they differ 
-        % in exactly two parameters.
-        frc_pairs = nchoosek(1:no_full_rank_choices, 2);
-        no_frc_pairs = size(frc_pairs, 1);
-        for pair = 1:no_frc_pairs
-            frc_diff = full_rank_participation(frc_pairs(pair, 1), :) - full_rank_participation(frc_pairs(pair, 2), :);
-            if sum(abs(frc_diff) > 0) == 2
-                linked_indices{index} = find(abs(frc_diff) == 1);
-                index = index + 1;
-                %   else
-                %     warning(' dimensions of parameter set ambiguous; data will be plotted as linear.')
-                %     clear linked_indices
-                %     linked_indices{1} = 1:no_varied;
-                %     return
-            end
-        end
-        
-    end
-
-  end
+  param_index = param_indices(p);
   
-  %% auto_gen_test_data_flag argout
-  if options.auto_gen_test_data_flag
-      argout = {linked_indices}; % specific to this function
-      
-      ds.unit.saveAutoGenTestDataLocalFn(argin, argout); % localfn
-  end
+  no_values_at_value1(p) = length(unique(params_at_value1(:, param_index)));
   
+end
+
+linked_indices = param_indices(no_values_at_value1 == 1);
+
+non_linked_indices = param_indices(no_values_at_value1 > 1);
+
+
+%% auto_gen_test_data_flag argout
+if options.auto_gen_test_data_flag
+  argout = {linked_indices, non_linked_indices}; % specific to this function
+  
+  ds.unit.saveAutoGenTestDataLocalFn(argin, argout); % localfn
+end
+
 end
