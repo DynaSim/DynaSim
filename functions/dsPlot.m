627108c1acfb6b972f1c8978602ec79e993817b4
benp <benpolletta@gmail.com>
2017-05-23 21:43:33 -0400

    Renaming checkCovary2 to checkCovary.

--- functions/dsPlot.m
+++ functions/dsPlot.m
@@ -195,7 +195,7 @@ if any(strcmp(fields, 'varied'))
     vary_lengths(v) = length(vary_vectors{v});
   end
 
-  [effective_vary_indices, ~] = ds.checkCovary2(vary_lengths, vary_params, varargin{:});
+  [effective_vary_indices, ~] = ds.checkCovary(vary_lengths, vary_params, varargin{:});
 
   if prod(vary_lengths(effective_vary_indices)) == length(data)
       
