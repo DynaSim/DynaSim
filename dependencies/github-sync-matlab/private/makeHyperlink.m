a94eb6ea1b4db2e40c3c6bf750cbd37666473e8d
Jason Sherfey <jssherfey@gmail.com>
2017-07-03 01:28:21 -0400

    moved dependencies into functions directory

--- dependencies/github-sync-matlab/private/makeHyperlink.m
+++
@@ -1,4 +0,0 @@
-function hyperlink = makeHyperlink(text, codeToRun)
-codeToRun = ['matlab: ' codeToRun];
-hyperlink = sprintf('<a href="%s">%s</a>', codeToRun, text);
-return
--- dependencies/github-sync-matlab/private/makeWeblinkCode.m
+++
@@ -1,3 +0,0 @@
-function weblinkCode = makeWeblinkCode(url)
-weblinkCode = sprintf('web(''%s'')', url);
-return
