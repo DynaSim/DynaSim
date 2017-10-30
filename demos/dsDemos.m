f56a7c7848e6c7a5d118e99fc23499c378ff0de2
Jason Sherfey <jssherfey@gmail.com>
2017-10-30 02:10:42 -0400

    made consistent with DyanSim paper

--- demos/dsDemos.m
+++ demos/dsDemos.m
@@ -44,12 +44,10 @@ eqns={
   'dx/dt=s*(y-x)'
   'dy/dt=r*x-y-x*z'
   'dz/dt=-b*z+x*y'
-  'w(t)=x.*y.*z'              % just an example to illustrate the use of monitors in DynaSim (monitor expression follows Matlab's syntax)
-  'monitor w'                 % indicate to store variable 'w' during simulation
 };
-data=dsSimulate(eqns, 'tspan',[0 100], 'ic',[1 2 .5], 'solver','rk4', 'study_dir','demo_lorenz');
+data=dsSimulate(eqns, 'time_limits',[0 100], 'ic',[1 2 .5], 'solver','rk4');
 
-% tspan: time limits on integration [ms]
+% time_limits: time limits on integration [ms]
 % ic: initial conditions
 % solver: numerical method to use (default: rk4 = "4th-order Runge-Kutta")
 
@@ -64,7 +62,7 @@ edit(data.simulator_options.solve_file)
 % function names.
 
 % Simulated data can be easily plotted using the resulting data structure:
-figure; plot(data.pop1_x,data.pop1_z);
+figure; plot(data.pop1_x,data.pop1_z); % <-- Figure 1 in DynaSim paper
 title('Lorenz equations'); xlabel('x'); ylabel('z')
 
 %% Izhikevich neuron with noisy drive
@@ -85,10 +83,10 @@ eqns={
   'I(t)=Iapp*(t>ton&t<toff).*(1+.5*rand(1,Npop))' % define applied input using reserved variables 't' for time and 'dt' for fixed time step of numerical integration
   'monitor I'                            % indicate to store applied input during simulation
 };
-data=dsSimulate(eqns, 'tspan',[0 1000], 'study_dir','demo_izhikevich');
+data=dsSimulate(eqns, 'time_limits',[0 1000]);
 
 % plot the simulated voltage and monitored input function
-figure;
+figure; % <-- Figure 2 in DynaSim paper
 subplot(2,1,1); plot(data.time,data.pop1_v); % plot voltage
 xlabel('time (ms)'); ylabel('v'); title('Izhikevich neuron')
 subplot(2,1,2); plot(data.time,data.pop1_I); % plot input function
@@ -130,11 +128,11 @@ vary={
   {P,'a',-.02;P,'b',-1 ; P,'c',-60; P,'d',8;  P,'I',80} % inhibition-induced spiking
   {P,'a',-.026;P,'b',-1; P,'c',-45; P,'d',0;  P,'I',70} % inhibition-induced bursting
   };
-data=dsSimulate(eqns, 'tspan',[0 250], 'vary',vary, 'study_dir','demo_izhikevich_vary');
+data=dsSimulate(eqns, 'time_limits',[0 250], 'vary',vary);
 dsPlot(data);
 
 % Do the same simulation, but using parfor in order to speed up larger computations
-data=dsSimulate(eqns, 'tspan',[0 250], 'vary',vary, 'study_dir','demo_izhikevich_vary_parallel', 'parallel_flag',1);
+data=dsSimulate(eqns, 'time_limits',[0 250], 'vary',vary, 'parallel_flag',1);
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% QUICKLY BUILDING LARGE MODELS FROM EXISTING "MECHANISMS"
@@ -163,13 +161,13 @@ eqns={
   'aN(v) = (.1-.01*(v+65))./(exp(1-.1*(v+65))-1)'
   'bN(v) = .125*exp(-(v+65)/80)'
 };
-data=dsSimulate(eqns, 'study_dir','demo_hh_1');
+data=dsSimulate(eqns);
 
 figure; plot(data.time,data.(data.labels{1}))
 xlabel('time (ms)'); ylabel('membrane potential (mV)'); title('Hodgkin-Huxley neuron')
 
 % Equivalent Hodgkin-Huxley neuron with predefined mechanisms
-data=dsSimulate('dv/dt=10+@current/Cm; Cm=1; v(0)=-65; {iNa,iK}', 'study_dir','demo_hh_2');
+data=dsSimulate('dv/dt=10+@current/Cm; Cm=1; v(0)=-65; {iNa,iK}');
 
 figure; plot(data.time,data.(data.labels{1}))
 xlabel('time (ms)'); ylabel('membrane potential (mV)'); title('Hodgkin-Huxley neuron')
@@ -180,16 +178,16 @@ xlabel('time (ms)'); ylabel('membrane potential (mV)'); title('Hodgkin-Huxley ne
 % Mechanisms can be custom built; however, DynaSim does come pakaged with
 % some common ones like popular ion currents (see <dynasim>/models).
 
-% Example of a bursting neuron model using three predefined current mechanisms:
+% Example of a bursting neuron model using three predefined current mechanisms
 eqns='dv/dt=Iapp+@current; {iNaF,iKDR,iM}; Iapp=5; gNaF=100; gKDR=5; gM=1.5; v(0)=-70';
-data=dsSimulate(eqns, 'tspan',[0 200]);
+data=dsSimulate(eqns, 'time_limits',[0 200]);
 
-figure; plot(data.time,data.(data.labels{1}))
+figure; plot(data.time,data.(data.labels{1})) % <-- Figure 3 in DynaSim paper
 xlabel('time (ms)'); ylabel('membrane potential (mV)'); title('Intrinsically Bursting neuron using predefined mechanisms')
 
 % Example of the same bursting neuron using a predefined population
 eqns='IB'; % file name of predefined model of intrinsically bursting (IB) neuron
-data=dsSimulate(eqns, 'vary',{'IB','Iapp',5}, 'tspan',[0 200]);
+data=dsSimulate(eqns, 'vary',{'IB','Iapp',5}, 'time_limits',[0 200]);
 
 figure; plot(data.time,data.(data.labels{1}))
 xlabel('time (ms)'); ylabel('membrane potential (mV)'); title('Predefined Intrinsically Bursting neuron')
@@ -227,9 +225,9 @@ s.connections(2).mechanism_list={'iAMPA'};
 s.connections(2).parameters={'tauD',2,'gSYN',.1,'netcon',ones(80,20)};
 
 %% Simulate Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
-data=dsSimulate(s, 'study_dir','demo_sPING_0');
+data=dsSimulate(s);
 
-dsPlot(data);
+dsPlot(data); % <--  4 in DynaSim paper
 dsPlot(data,'variable',{'E_v','E_I_iGABAa_ISYN'});
 
 % View the connection mechanism file:
@@ -279,7 +277,7 @@ dsSimulate(s, 'save_data_flag', 1, 'study_dir', 'demo_sPING_3',...
                 'vary', vary, 'verbose_flag', 1);
 data=dsImport('demo_sPING_3');
 dsPlot(data);
-dsPlot(data,'plot_type','rastergram');
+dsPlot(data,'plot_type','rastergram'); % <-- Figure 5 in DynaSim paper
 dsPlotFR(data); % examine how mean firing rate changes with Iapp and tauD
 
 dsPlot(data,'plot_type','power');
@@ -421,3 +419,81 @@ s.connections(2).direction='FS->RS';          % I->E connection
 s.connections(2).mechanism_list='iGABAa';     % GABAa synapse
 s.connections(2).parameters={'gSYN',1,'tauD',10}; % strength and time constant of inhibition
 dsPlot(dsSimulate(s,'time_limits',T));
+
+%% Multicompartment neurons
+
+% Excitatory cell S driving two-comparmtent E-cell
+s=[];
+s.populations(1).name='S';
+s.populations(1).equations='dv/dt=@current+10;{iNa,iK}';
+s.compartments(1).name='Edend';
+s.compartments(1).equations='dv/dt=@current;{iNa,iK}';
+s.compartments(2).name='Esoma';
+s.compartments(2).equations='dv/dt=@current;{iNa,iK}';
+s.connections(1).direction='S->Edend';
+s.connections(1).mechanism_list='iAMPA';
+s.connections(2).direction='Edend->Esoma';
+s.connections(2).mechanism_list='iCOM';
+
+d=dsSimulate(s);
+dsPlot(d);
+
+% for more examples, see: dynasim/demos/examples/Multicompartment_PFC_neurons
+
+%% Networks of integrate-and-fire neurons with axonal delays and refractory period
+% tau [ms]: membrane time constant (RC)
+% tabs [ms]: absolute refractory period
+% delay [ms]: axonal delay
+% tspike: reserved variable for storing past spike times when monitoring spikes
+
+% 1 E-cell driving 1 I-cell
+LIF={
+    'dV/dt=(E-V+R*I+noise*randn-@isyn)/tau; V(0)=-65'
+    'if(any(t<tspike+tabs,1))(V=reset)'
+    'tau=10; tabs=10; E=-70; thresh=-55; reset=-75; R=9; I=1.55; noise=100'
+    'monitor V.spikes(thresh)'
+     };
+
+iampa={
+  'gSYN=.5; ESYN=0; tauD=2; tauR=0.4; delay=15'
+  'f(x) = (exp(-x/tauD)-exp(-x/tauR)).*(x>0)'
+  'Isyn(V) = gSYN.*sum(f(t-tspike_pre-delay)).*(V-ESYN)'
+  '@isyn += Isyn(V_post)'
+};
+
+s=[];
+s.populations(1).name='E';
+s.populations(1).equations=LIF;
+s.populations(2).name='I';
+s.populations(2).equations=LIF;
+s.populations(2).parameters={'I',0,'noise',0};
+s.connections(1).direction='E->I';
+s.connections(1).mechanism_list='iampa';
+s.mechanisms(1).name='iampa';
+s.mechanisms(1).equations=iampa;
+
+data=dsSimulate(s,'time_limits',[0 200],'solver','rk1','dt',.01);
+dsPlot(data);
+
+% for more examples, see: dynasim/demos/dsLIFnetwork.m
+
+%% Delay differential equations (e.g., axonal delays in network of HH neurons)
+
+ampa_with_delay={
+  'gSYN=.1; ESYN=0; tauD=2; tauR=0.4; delay=20'
+  'netcon=ones(N_pre,N_post)'
+  'ISYN(X,s)=gSYN.*(s*netcon).*(X-ESYN)'
+  'ds/dt=-s./tauD+((1-s)/tauR).*(1+tanh(X_pre(t-delay)/10)); s(0)=.1' % 20ms delay
+  '@current += -ISYN(X_post,s)'
+};
+
+s=[];
+s.populations(1).name='HH';
+s.populations(1).equations='dV/dt=@current+10*(t<50);{iNa,iK};V(0)=-65';
+s.connections(1).direction='HH->HH';
+s.connections(1).mechanism_list='iampa';
+s.connections(1).parameters={'gSYN',.1,'netcon',netcon};
+s.mechanisms(1).name='iampa';
+s.mechanisms(1).equations=ampa_with_delay;
+data=dsSimulate(s,'time_limits',[0 100]);
+figure; plot(data.time,data.HH_V);
