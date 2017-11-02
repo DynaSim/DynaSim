6ce13f6a1ca272503cef8c430c9b2e00940a3d39
Jason Sherfey <jssherfey@gmail.com>
2017-11-02 16:33:43 -0400

    clarified the importance of matCompatibility_flag

--- demos/dsDemos.m
+++ demos/dsDemos.m
@@ -249,15 +249,14 @@ dynasim(s); % Display model "s" in the DynaSim GUI
 
 %% Save data from a single simulation
 
-% By default data is saved in compatible mode between Matlab and Octave ('matCompatibility_flag' set to 1, i.e., data is saved in '-v7' mat format).
-
 % Example using the previous sPING model:
 data=dsSimulate(s,'save_data_flag',1,'study_dir','demo_sPING_1');
 
-%% Unfortunately, '-v7' mat format is not able to save variables > 2GB.
-% If compatible saving fails, data is saved in '-v7.3' format (Matlab), or in '-hdf5' format (Octave). This allows that data can be stored in all its integrity.
-
-%% However, if any var > 2GB is anticipated, we strongly recommend to set 'matCompatibility_flag' manually to 0 to speed up data saving.
+% Saving large data files:
+% - By default data is saved in compatible mode between Matlab and Octave ('matCompatibility_flag' set to 1, i.e., data is saved in '-v7' mat format).
+%   - Unfortunately, '-v7' mat format is not able to save variables > 2GB.
+%   - If compatible saving fails, data is saved in '-v7.3' format (Matlab), or in '-hdf5' format (Octave). This allows that data can be stored in all its integrity.
+% - However, if any var > 2GB is anticipated, we strongly recommend to set 'matCompatibility_flag' manually to 0 to speed up data saving.
 % Example using 'matCompatibility_flag' set to 0:
 data=dsSimulate(s,'matCompatibility_flag',0,'save_data_flag',1,'study_dir','demo_sPING_1_varlt2GB');
 
