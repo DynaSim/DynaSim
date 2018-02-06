95a11f41dc84ef9126710051110913441e4e143f
Erik Roberts <eriktheblue@gmail.com>
2018-02-06 09:23:40 -0500

    rename vars

--- functions/internal/dsNameFromVaried.m
+++ functions/internal/dsNameFromVaried.m
@@ -1,23 +1,23 @@
-function new_result_file = dsNameFromVaried(data, file_type, old_result_file)
+function new_filename = dsNameFromVaried(data, prefix, old_filename)
 %NAMEFROMVARIED - makes a filename based on the parameters in data.varied.
 %
 % Usage:
-%   new_result_file = dsNameFromVaried(data, file_type, old_result_file)
+%   new_filename = dsNameFromVaried(data, file_type, old_filename)
 %
 % Inputs:
 %   - data: DynaSim data structure (also accepted: data file name)
-%   - file_type: file prefix for type of file
+%   - prefix: file prefix for type of file
 %   - result_file: previous result_file, only uses this for the path.
 %
 % Outputs:
-%   - new_result_file: where to save result, based on file_type and data.varied
+%   - new_filename: where to save result, based on file_type and data.varied
 
-pathstr = fileparts2(old_result_file);
+pathstr = fileparts2(old_filename);
 
-fileName = file_type;
+fileName = prefix;
 
 %check for simID# from batch sims
-token = regexp(old_result_file, '(sim\d+)', 'tokens');
+token = regexp(old_filename, '(sim\d+)', 'tokens');
 if ~isempty(token)
   fileName = [fileName '_' token{1}{1}];
 end
@@ -26,6 +26,6 @@ end
     fileName = [fileName '__' param{1} '_' sprintf('%g',data.(param{1}))];
   end
 
-new_result_file = fullfile(pathstr, fileName);
+new_filename = fullfile(pathstr, fileName);
 
 end
