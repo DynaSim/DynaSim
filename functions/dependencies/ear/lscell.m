c3ffd3c10a52d4c13ba077384578d247e1428195
Erik Roberts <eriktheblue@gmail.com>
2018-02-14 13:20:27 -0500

    update lscell

--- functions/dependencies/ear/lscell.m
+++ functions/dependencies/ear/lscell.m
@@ -1,74 +1,101 @@
-function dirList = lscell(arg, removePathBool, recursiveBool)
+function dirList = lscell(arg, removePathBool, relativePathBool)
 %% lscell
-% Author: Erik Roberts
+% Author: Erik Roberts, 2018
 %
-% Purpose: returns cell matrix of strings with results from call to ls/dir
+% Purpose: returns cell matrix of strings with results from call to dir
 %
 % Usage: dirList = lscell()
 %        dirList = lscell(arg)
 %        dirList = lscell(arg, removePathBool)
+%        dirList = lscell(arg, removePathBool, relativePathBool)
 %
 % Inputs (optional):
-%   arg: argument to ls
-%   removePathBool: logical of whether to remove the path before the files/dirs
+%   arg: argument to dir
+%   removePathBool: logical whether to remove the path before the files/dirs (default=true)
+%   relativePathBool: logical whether to convert absolute paths to relative paths (default=false)
+%                     if removePathBool== true, this is ignored.
+%
+% Output:
+%   dirList: cellstr list of arg contents from dir. Paths to folders never
+%            end in trailing filesep, i.e. '/' or '\'.
+%
+% Tips: in order to search subdirectories, use the '**' glob character in the arg
+%
+% See also: DIR
 
-% args
+% Dev Note: checked on Mac OS 10.12, Windows 10, Linux with Matlab 2017b
+
+% parse args
+if ~nargin || isempty(arg)
+  arg = '.';
+end
 if nargin < 2 || isempty(removePathBool)
   removePathBool = true; %defaults to true
 end
-if nargin < 3 || isempty(recursiveBool)
-  recursiveBool = false; %defaults to false
-end
-if ~nargin || isempty(arg)
-  if ~recursiveBool
-    arg = {};
-  else
-    arg = {'.'};
-  end
-else
-  arg = {arg};
-end
-  
-if ~recursiveBool
-  try
-    dirList = ls(arg{:});
-  catch % no arg files found
-    dirList = {};
-    return
-  end
-else % recursiveBool
-  if (ismac || isunix)
-    [~, dirList] = system(['find ' arg{:}]);
-  elseif ispc
-  end
+if nargin < 3 || isempty(relativePathBool)
+  relativePathBool = false; %defaults to false
 end
 
-if (ismac || isunix)
-  dirList = strsplit(dirList);
-  if isempty(dirList{end})
-    dirList(end) = [];
-  end
-elseif ispc
-  dirList = cellstr(dirList);
-  if strcmp(dirList{1},'.')
-    dirList(1:2) = []; %remove leading . and ..
-  end
-end
+% get dir contents
+dirListS = dir(arg);
 
-if isempty(dirList) || isempty(dirList{1})
+if isempty(dirListS)
   dirList = {};
+  return
 end
 
-if exist('removePathBool','var') && removePathBool
-  dirList = removePath(dirList);
+% remove first period
+if strcmp(dirListS(1).name, '.')
+  dirListS(1) = [];
+end
+
+% remove double period
+if strcmp(dirListS(1).name, '..')
+  dirListS(1) = [];
+end
+
+nFiles = length(dirListS);
+
+% convert struct to cellstr
+dirList = cell(nFiles,1);
+for k = 1:nFiles
+  dirList{k} = fullfile(dirListS(k).folder, dirListS(k).name);
+end
+
+% remove extra cells with '..'
+  dirList(~cellfun(@isempty, regexp(dirList, '\.\.$'))) = [];
+
+% remove trailing period and filesep from dirs
+if isunix || ismac
+  dirList = regexprep(dirList, '/\.$', '');
+else
+  dirList = regexprep(dirList, '\\\.$', '');
+end
+
+% remove duplicated absolute paths from glob
+dirList = unique(dirList);
+
+% dirList is cellstr with absolute paths
+
+if relativePathBool && ~removePathBool
+  regexStr = ['^' pwd filesep];
+  if isunix || ismac
+    dirList = regexprep(dirList, regexStr, '');
+  else
+    regexStr = strrep(regexStr, '\', '\\');
+    dirList = regexprep(dirList, regexStr, '');
+  end
+end
+
+% dirList is cellstr with absolute paths, or relative paths starting with name
+
+if removePathBool
+  dirList = cellfun(@removePath, dirList, 'Uni',0);
 end
   
-  % sub functions
-  function dirList = removePath(dirList)
-    for k = 1:length(dirList)
-      thisDir = dirList{k};
-      [~,name,ext] = fileparts(thisDir);
-      dirList{k} = [name,ext];
-    end
+  % nested functions
+  function thisFilename = removePath(thisPath)
+    [~,name,ext] = fileparts(thisPath);
+    thisFilename = [name,ext];
   end
 end
