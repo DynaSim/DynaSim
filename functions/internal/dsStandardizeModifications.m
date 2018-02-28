799d72e93dc00ce097160db9fc61a434d4da3e77
Erik Roberts <eriktheblue@gmail.com>
2018-02-28 10:44:03 -0500

    close #413

--- functions/internal/dsStandardizeModifications.m
+++ functions/internal/dsStandardizeModifications.m
@@ -1,4 +1,4 @@
-function modifications = dsStandardizeModifications(modifications,specification, varargin)
+function [modifications, identLinkedMods] = dsStandardizeModifications(modifications,specification, varargin)
 % dsStandardizeModifications - convert all modifications into 3-column cell
 %                              matrix format (namespace,variable,value) and expand
 %
@@ -12,6 +12,11 @@ function modifications = dsStandardizeModifications(modifications,specification,
 % Inputs (optional):
 %   specification: needed if namespace empty to check for first population name.
 %                  just need "specification.populations(1).name)".
+%
+% Outputs:
+%   modifications: standardized, expanded modifcations
+%   identLinkedMods: cell array of indicies of which mods are identically
+%                    linked/covaried, where each cell is a diff linked set
 
 % Dev Notes:
 %   E.R. Feb 2018: Moved to separate function
@@ -36,7 +41,7 @@ if isstruct(modifications)
 end
 
 % check backward compatibility
-modifications=backward_compatibility(modifications);
+modifications = backward_compatibility(modifications);
 
 % check for empty object specification
 missing_objects=find(cellfun(@isempty,modifications(:,1)));
@@ -46,49 +51,65 @@ if any(missing_objects)
   end
   
   % set to default population name
-  for i=1:length(missing_objects)
-    modifications{missing_objects(i),1}=specification.populations(1).name;%'pop1';
+  for iObj = 1:length(missing_objects)
+    modifications{missing_objects(iObj),1} = specification.populations(1).name;
   end
 end
 
 % support modifying multiple elements (of namespace or variable) simultaneously
 % approach -- add extra entry for each thing to modify
-% eg) expand {'(E,I)','Cm',2} to {'E','Cm',2; 'I','Cm',2}
+% eg: expand {'(E,I)','Cm',2} to {'E','Cm',2; 'I','Cm',2}
 % note: should be able to support {'(E,I)','(EK,EK2)',-80}
 if any(~cellfun(@isempty,regexp(modifications(:,1),'^\(.*\)$'))) || ...
     any(~cellfun(@isempty,regexp(modifications(:,2),'^\(.*\)$')))
   
   % loop over modifications
-  modifications_={};
-  for i=1:size(modifications,1)
+  modifications_expanded = {};
+  identLinkedMods = {};
+  for iMod = 1:size(modifications,1)
     % check namespace for ()
-    namespaces=regexp(modifications{i,1},'[\w\.\-<>]+','match');
+    namespaces = regexp(modifications{iMod,1},'[\w\.\-<>]+','match');
     
     % check variable for ()
-    variables=regexp(modifications{i,2},'[\w\.-]+','match');
+    variables = regexp(modifications{iMod,2},'[\w\.-]+','match');
     
-    if ischar(modifications{i,3})
+    thisModStartInd = size(modifications_expanded, 1) + 1;
+    
+    if ischar(modifications{iMod,3})
       
       % expand list of modifications
-      for j=1:length(namespaces)
-        for k=1:length(variables)
-          modifications_(end+1,1:3)={namespaces{j},variables{k},modifications{i,3}};
+      for iNamespace = 1:length(namespaces)
+        for iVar = 1:length(variables)
+          modifications_expanded(end+1,1:3)={namespaces{iNamespace},variables{iVar},modifications{iMod,3}};
         end
       end
       
-    elseif isnumeric(modifications{i,3})
+      % check for identical covary
+     if length(variables)>1 || length(namespaces)>1
+       % determine identLinkedMods
+       identLinkedMods{end+1} = thisModStartInd : size(modifications_expanded, 1);
+     end
+      
+    elseif isnumeric(modifications{iMod,3})
       
       % check size of values matches number of namespaces, variables
-      if isscalar(modifications{i,3}) % in case number of values is one
-        modifications{i,3} = repmat(modifications{i,3},length(variables),length(namespaces));
+      if isscalar(modifications{iMod,3}) % in case number of values is one
+        modifications{iMod,3} = repmat(modifications{iMod,3},length(variables),length(namespaces));
+        
+        % check for identical covary
+        if length(variables)>1 || length(namespaces)>1
+          identCovaryBool = true;
+        else
+          identCovaryBool = false;
+        end
       else
-        if size(modifications{i,3},1) ~= length(variables) || size(modifications{i,3},2) ~= length(namespaces)
+        if size(modifications{iMod,3},1) ~= length(variables) || size(modifications{iMod,3},2) ~= length(namespaces)
           % in case values is number of variables x 1
-          if size(modifications{i,3},1) == length(variables) && size(modifications{i,3},2) == 1
-            modifications{i,3} = repmat(modifications{i,3},1,length(namespaces));
+          if size(modifications{iMod,3},1) == length(variables) && size(modifications{iMod,3},2) == 1
+            modifications{iMod,3} = repmat(modifications{iMod,3},1,length(namespaces));
             % in case values is 1 x number of namespaces
-          elseif size(modifications{i,3},2) == length(namespaces) && size(modifications{i,3},1) == 1
-            modifications{i,3} = repmat(modifications{i,3},length(variables),1);
+          elseif size(modifications{iMod,3},2) == length(namespaces) && size(modifications{iMod,3},1) == 1
+            modifications{iMod,3} = repmat(modifications{iMod,3},length(variables),1);
             % TODO: char inputs
             % elseif ischar(modifications{i,3})
             % string input
@@ -97,27 +118,35 @@ if any(~cellfun(@isempty,regexp(modifications(:,1),'^\(.*\)$'))) || ...
               'where dimensions 1, 2, and 3 correspond to mechanisms, values, and populations varied over.'])
           end
         end
+        
+        identCovaryBool = false;
       end
       
       % expand list of modifications
-      for j=1:length(namespaces)
-        for k=1:length(variables)
-          modifications_(end+1,1:3)={namespaces{j},variables{k},modifications{i,3}(k,j)};
+      for iNamespace = 1:length(namespaces)
+        for iVar = 1:length(variables)
+          modifications_expanded(end+1,1:3)={namespaces{iNamespace}, variables{iVar}, modifications{iMod,3}(iVar,iNamespace)};
         end
       end
+      
+     % determine identLinkedMods
+     if identCovaryBool
+       identLinkedMods{end+1} = thisModStartInd : size(modifications_expanded, 1);
+     end
       
     end % ischar
     
   end % mods
   
-  modifications=modifications_;
+  % rename
+  modifications = modifications_expanded;
   
 end % if groups
 
 % standardize connection object
-for i = 1:size(modifications,1)
-  obj=modifications{i,1}; % population name or connection source-target
-  fld=modifications{i,2}; % population name, size, or parameter name
+for iMod = 1:size(modifications,1)
+  obj=modifications{iMod,1}; % population name or connection source-target
+  fld=modifications{iMod,2}; % population name, size, or parameter name
   
   % convert target<-source to source->target
   if any(strfind(obj,'<-'))
@@ -133,7 +162,7 @@ for i = 1:size(modifications,1)
     obj=[obj suffix];
     
     % update modifications
-    modifications{i,1} = obj; % population name or connection source-target
+    modifications{iMod,1} = obj; % population name or connection source-target
   end
   
   % check for dot notation. Change POP.MECH to MECH_PARAM notation.
@@ -144,8 +173,8 @@ for i = 1:size(modifications,1)
     fld=[MECH '_' fld];
     
     % update modifications
-    modifications{i,1} = obj; % population name or connection source-target
-    modifications{i,2} = fld; % population name, size, or parameter name
+    modifications{iMod,1} = obj; % population name or connection source-target
+    modifications{iMod,2} = fld; % population name, size, or parameter name
   end
   
   % check for dot notation. Change MECH.PARAM to MECH_PARAM notation.
@@ -154,7 +183,7 @@ for i = 1:size(modifications,1)
     fld=[tmp{1} '_' tmp{2}];
     
     % update modifications
-    modifications{i,2} = fld; % population name, size, or parameter name
+    modifications{iMod,2} = fld; % population name, size, or parameter name
   end
 end
 
@@ -174,23 +203,23 @@ function modifications = backward_compatibility(modifications)
 % convert 2-column specification to 3-column specification with empty object name
 if size(modifications,2)==2
   tmp={};
-  for i=1:size(modifications,1)
-    tmp{i,1}='';
-    tmp{i,2}=modifications{i,1};
-    tmp{i,3}=modifications{i,2};
+  for iMod = 1:size(modifications,1)
+    tmp{iMod,1}='';
+    tmp{iMod,2}=modifications{iMod,1};
+    tmp{iMod,3}=modifications{iMod,2};
   end
   modifications=tmp;
 end
 
 % convert 4-column specification to 3-column
 if size(modifications,2)==4
-  for i=1:size(modifications,1)
-    if strcmp(modifications{i,2},'parameters')
+  for iMod = 1:size(modifications,1)
+    if strcmp(modifications{iMod,2},'parameters')
       % shift parameter name to 2nd column
-      modifications{i,2}=modifications{i,3};
+      modifications{iMod,2}=modifications{iMod,3};
 
       % shift parameter value to 3rd column
-      modifications{i,3}=modifications{i,4};
+      modifications{iMod,3}=modifications{iMod,4};
     end
   end
 
@@ -202,8 +231,8 @@ end
 if any(~cellfun(@isempty,regexp(modifications(:,1),'\w-\w')))
   % find elements to adjust
   inds=find(~cellfun(@isempty,regexp(modifications(:,1),'\w-\w')));
-  for i=1:length(inds)
-    modifications{inds(i),1}=strrep(modifications{inds(i),1},'-','->');
+  for iMod=1:length(inds)
+    modifications{inds(iMod),1}=strrep(modifications{inds(iMod),1},'-','->');
   end
 end
 
