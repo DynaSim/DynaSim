5f3dc354ee7fec1a87dbb21ed20ab9f0cdca39da
Salva Ardid <jsardid@gmail.com>
2019-01-29 12:47:40 -0500

    fixed a bug in UIverlessthan.m

--- functions/dependencies/UIverlessthan.m
+++ functions/dependencies/UIverlessthan.m
@@ -1,28 +1,17 @@
 function result = UIverlessthan (ref_version);
   UIversion = version;
-  [UIversion,UIversion2] = versionReplaceDots(UIversion);
-  UIversion = hex2dec(UIversion);
-  [ref_version,ref_version2] = versionReplaceDots(ref_version);
-  ref_version = hex2dec(ref_version);
-  result = UIversion < ref_version;
-  if ~result
-    UIversion2 = hex2dec(UIversion2);
-    ref_version2 = hex2dec(ref_version2);
-    result = UIversion2 < ref_version2;
-  end
-end
+  UIversion_strparts = strsplit(UIversion,'.');
+  ref_version_strparts = strsplit(ref_version,'.');
 
-function [versionNoDots,versionNoDots2] = versionReplaceDots(versionDots)
-  versionDots_parts = strsplit(versionDots,'.');
-  versionNoDots2 = '0';
-  switch numel(versionDots_parts)
-    case 1, versionNoDots = [[versionDots_parts{:}],'00'];
-    case 2, versionNoDots = [[versionDots_parts{:}],'0'];
-    case 3, versionNoDots = [versionDots_parts{:}];
-    case 4,
-      versionNoDots = [versionDots_parts{1:3}];
-      tmp = strsplit(versionDots_parts{4},' ');
-      versionNoDots2 = [tmp{1}];
-    otherwise error('version wrong format');
+  for ii = 1:min([numel(UIversion_strparts),numel(ref_version_strparts)])
+    UIversion_part = hex2dec(UIversion_strparts{ii});
+    ref_version_part = hex2dec(ref_version_strparts{ii});
+    if UIversion_part ~= ref_version_part
+      result = UIversion_part < ref_version_part;
+      return
+    end
   end
+
+  % if everything the same but different number of parts
+  result = numel(UIversion_strparts) < numel(ref_version_strparts);
 end
