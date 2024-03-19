1830b8a2d3b7fe27f455d91d34507a3364e97d7e
Nejat <hamed.nejat@vanderbilt.edu>
2024-03-19 11:29:05 -0500

    V2.5.8

---
+++ functions/dynalearn/dlLogCorrelationDivergence.m
@@ -0,0 +1,44 @@
+function d = dlLogCorrelationDivergence(p, q)
+
+    n = length(p);
+    m = length(q);
+    N = linspace(0, 1, n);
+    M = linspace(0, 1, m);
+
+    if isnan(p)
+
+        d = 1e+6;
+        return;
+
+    elseif isinf(p)
+
+        d = 1e+6;
+        return;
+        
+    end
+
+    if n > m
+
+        dlQ = interp1(M, q, N);
+        dlP = p / sum(p);
+        dlQ = dlQ / sum(dlQ);
+
+    elseif m > n
+
+        dlP = interp1(N, p, M);
+        dlP = dlP / sum(dlP);
+        dlQ = q / sum(q);
+
+    else
+
+        dlP = p / sum(p);
+        dlQ = q / sum(q);
+
+    end
+
+    d = abs(log(dlP ./ dlQ));
+    d(isnan(d)) = 0;
+    d(isinf(d)) = max(n, m);
+    d = mean(d);
+
+end
---
+++ functions/dynalearn/dlPowerSpectrumLogCorrelation.m
@@ -0,0 +1,33 @@
+function d = dlPowerSpectrumLogCorrelation(dlObj, opts)
+
+    dlPotentialIndices = endsWith(dlObj.dlVariables, opts.id);
+    dlPotentials = dlObj.dlOutputs(dlPotentialIndices);
+    dlFs = floor(1000/(dlObj.dldT * dlObj.dlDownSampleFactor));  
+    dlQ = opts.target;
+
+    n = size(dlPotentials, 2);
+    x = dlPotentials{1, 1};
+    L = size(x, 1);
+
+    dlLf = 1 + floor((opts.lf / dlFs)*L);
+    dlHf = floor((opts.hf / dlFs)*L);
+    y = zeros(n, L);
+            
+    for k = 1:n
+        
+        x = dlPotentials{1, k};
+        N = size(x, 2);
+        x = mean(x, 2);
+
+        x = detrend(x);
+        [Y, ~] = dlSpectrum(x, dlFs);
+        y(k, :) = Y*N;
+        
+    end
+    
+    dlP = mean(y, 1);
+    dlP = dlP(dlLf:dlHf);
+    d = dlLogCorrelationDivergence(dlP, dlQ);
+
+end
+
