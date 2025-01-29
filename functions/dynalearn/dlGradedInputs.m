8d9fbeef0f44889d8e2cc635932e943bec1b9633
Nejat <hamed.nejat@vanderbilt.edu>
2025-01-29 17:22:44 -0600

    V8.2.0

--- functions/dynalearn/dlGradedInputs.m
+++ functions/dynalearn/dlGradedInputs.m
@@ -10,6 +10,11 @@ function trialParams = dlGradedInputs(duration, t0, t1, Iappmx, n)
         trialParams{i}('EXc_Iapp') = Iappmx*((i-1)/(n));
         trialParams{i}('INg_Iapp') = Iappmx*((i-1)/(n));
         trialParams{i}('INl_Iapp') = Iappmx*((i-1)/(n));
+
+
+        trialParams{i}('EXc_omega') = 0.12;
+        trialParams{i}('INg_omega') = 0.12;
+        trialParams{i}('INl_omega') = 0.12;
     
         trialParams{i}('EXc_t0') = t0;
         trialParams{i}('EXc_t1') = t1;
